a931607a250dd9650279ff78c265cfb5c2bc64be
inffy <juha@uotila.org>
2022-09-29 09:10:44 +0300

    add lulu and saka68

---
+++ keyboards/boardsource/lulu/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7F, 0x59, 0x9E, 0xBB, 0x0D, 0x7C, 0xAF, 0x1D}
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
---
+++ keyboards/cmm_studio/saka68/hotswap/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x00, 0xF5, 0x66, 0x62, 0xF7, 0x8E, 0x28, 0xD3}
+#define QMK_KEYS_PER_SCAN 8
+#define DEBOUNCE 5
+#define USB_POLLING_INTERVAL_MS 1
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
